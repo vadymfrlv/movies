b6358db05eafd5082bacbbce37c461ba3cc65eff
Vadym Frolov <vdfroloff@gmail.com>
2023-05-30 22:45:24 +0300

    INIT: vite config + dependencies

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      components: '/src/components',
+      pages: '/src/pages',
+      services: '/src/services',
+      utils: '/src/utils',
+    },
+  },
+});
